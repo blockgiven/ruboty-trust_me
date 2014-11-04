2a85abf1a45a48abb293cca651f0de162ef3c6c3
block_given? <block_given@outlook.com>
2014-11-04 22:18:22 +0900

    fix don't trust

--- lib/ruboty/trust_me/actions/dont_trust.rb
+++ lib/ruboty/trust_me/actions/dont_trust.rb
@@ -4,10 +4,10 @@ module Ruboty
       class DontTrust < Ruboty::Actions::Base
         def call
           if message[:name]
-            message.robot.trusted_names -=  [message[:name]]
+            message.robot.trusted_names.delete_if {|n| n == message[:name] }
             message.reply(%(I don't trust #{message[:name]}.))
           else
-            message.robot.trusted_names -= [message.from_name]
+            message.robot.trusted_names.delete_if {|n| n == message.from_name }
             message.reply(%(I don't trust you.'))
           end
         end
